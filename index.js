8bab18500679c12795d92e586ddea9c335279de4
Saiee-phadatare <ssp.saiee@gmail.com>
2025-03-09 18:17:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const rateLimit = require("express-rate-limit");
 const cors = require('cors');
 const helmet = require('helmet');
 const cookieParser = require("cookie-parser");
@@ -10,9 +11,14 @@ const authRouter = require("./routers/authRouter");
 const postRouter = require("./routers/postRouter");
 
 const app = express();
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // Limit each IP to 100 requests per window
+});
 
 //Middleware
 app.use(cors());
+app.use(limiter);
 app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
